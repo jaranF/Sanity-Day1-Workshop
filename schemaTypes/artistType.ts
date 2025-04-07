a3872f03c93b2f0feae7954d855031f7cc24d8b5
JaranF <jaranf@nowheredomain.com>
2025-04-07 11:37:39 +0100

    feat: (experiment) Again, with 'title' ommited, the 'name' is picked up &
     used in the left-hand navigation of Sanity Studio, but is capitlized on each letter that appears after the underscore in the snakecase notation. PS You cannout have any spaces in the 'name' field

--- schemaTypes/artistType.ts
+++ schemaTypes/artistType.ts
@@ -1,7 +1,7 @@
 import {defineField, defineType} from "sanity";
 
 export const artistType = defineType({
-  name: "blahrtist",
+  name: "artist_of_the_week",
   type: "document",
   fields: [
     defineField({
